5117f8c3dcfd06cde8cf06e27f4c15d46eb398ae
Daniel Candia <danfercf@gmail.com>
2022-02-10 17:36:40 -0400

    Add SquaredArray

---
+++ sorted-squared.js
@@ -0,0 +1,8 @@
+function squaredArray(object) {
+	let squaredArray = [];
+  squaredArray = object.array.map(value => {
+  	return value * value;
+  });
+  
+  return squaredArray.sort((a, b) => {return a - b});
+}
